d8f01b2fb0aee80d6c562d0d8f68feb26fc78b00
Vickenty Fesunov <kent@setattr.net>
2022-11-11 22:31:03 +0100

    Compare original and nostd versions

--- ahrs-py/src/lib.rs
+++ ahrs-py/src/lib.rs
@@ -1,22 +1,62 @@
 use pyo3::prelude::*;
 
 #[pyclass]
-struct MargEkf {
-    inner: ahrs::MargEkf,
+struct MargEkfFork {
+    inner: ahrs_fork::MargEkf,
 }
 
 #[pymethods]
-impl MargEkf {
+impl MargEkfFork {
     #[new]
-    fn new() -> MargEkf {
-        MargEkf {
-            inner: ahrs::MargEkf::new(),
+    fn new() -> MargEkfFork {
+        MargEkfFork {
+            inner: ahrs_fork::MargEkf::new(),
         }
     }
 
     #[getter]
     fn state(&self) -> PyResult<[f32; 7]> {
         Ok(self.inner.state[0])
+    }
+
+    #[getter]
+    fn predicted_accel_mag(&self) -> PyResult<[f32; 6]> {
+        Ok(self.inner.predicted_accel_mag[0])
+    }
+
+    fn predict(&mut self, w_x: f32, w_y: f32, w_z: f32, dt: f32) -> PyResult<()> {
+        self.inner.predict(w_x, w_y, w_z, dt);
+        Ok(())
+    }
+
+    fn update(&mut self, accel: [f32; 3], mag: [f32; 3]) -> PyResult<()> {
+        self.inner.update(accel, mag);
+        Ok(())
+    }
+}
+
+#[pyclass]
+struct MargEkfBofh {
+    inner: ahrs_bofh::MargEkf,
+}
+
+#[pymethods]
+impl MargEkfBofh {
+    #[new]
+    fn new() -> MargEkfBofh {
+        MargEkfBofh {
+            inner: ahrs_bofh::MargEkf::new(),
+        }
+    }
+
+    #[getter]
+    fn state(&self) -> PyResult<Vec<f32>> {
+        Ok(self.inner.state.to_vec())
+    }
+
+    #[getter]
+    fn predicted_accel_mag(&self) -> PyResult<Vec<f32>> {
+        Ok(self.inner.predicted_accel_mag.to_vec())
     }
 
     fn predict(&mut self, w_x: f32, w_y: f32, w_z: f32, dt: f32) -> PyResult<()> {
@@ -33,6 +73,7 @@ impl MargEkf {
 
 #[pymodule]
 fn ahrs_py(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<MargEkf>()?;
+    m.add_class::<MargEkfFork>()?;
+    m.add_class::<MargEkfBofh>()?;
     Ok(())
 }
